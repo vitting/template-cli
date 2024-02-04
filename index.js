7a19d41334562190fb99e9beeb8a376b3838386f
Christian Nicolaisen <cvn_vitting@hotmail.com>
2024-02-04 12:31:24 +0100

    initial commit

--- index.js
+++ index.js
@@ -2,9 +2,8 @@
 
 import inquirer from "inquirer";
 import * as fs from "fs";
-import path, { dirname } from "path";
-import { fileURLToPath } from "url";
-import createDirectoryContents from "./createDirectoryContents.js";
+import path from "path";
+// import { fileURLToPath } from "url";
 
 const currentDir = process.cwd();
 // const __dirname = dirname(fileURLToPath(import.meta.url));
@@ -33,22 +32,35 @@ const QUESTIONS = [
   },
 ];
 
-inquirer.prompt(QUESTIONS).then((answers) => {
-  // template dir chosen
-  const projectChoice = answers["template-choice"];
+const answers = await inquirer.prompt(QUESTIONS);
 
-  // user input of project name
-  const projectName = answers["project-name"];
+//template dir chosen
+const projectChoice = answers["template-choice"];
 
-  // Full path to the chosen template dir
-  const templatePath = `${templateDir}/${projectChoice}`;
+// user input of project name
+const projectName = answers["project-name"];
 
-  // Path to create the new project directory
-  const projectDir = path.resolve(currentDir, projectName);
+// Full path to the chosen template dir
+const templatePath = `${templateDir}/${projectChoice}`;
 
-  // Create the project directory with the project name.
-  fs.mkdirSync(projectDir, { recursive: true });
-  // Copy the files from the
-  fs.cpSync(templatePath, projectDir, { recursive: true });
-  // createDirectoryContents(templatePath, projectName);
-});
+// Path to create the new project directory
+const projectDir = path.resolve(currentDir, projectName);
+
+// Create the project directory with the project name.
+fs.mkdirSync(projectDir, { recursive: true });
+// Copy the files from the
+fs.cpSync(templatePath, projectDir, { recursive: true });
+
+// Read package.json from new project and change name to chosen project name
+const data = fs.readFileSync(`${projectDir}/package.json`, "utf8");
+const projectPackageJson = JSON.parse(data);
+
+projectPackageJson.name = projectName;
+
+fs.writeFileSync(
+  path.join(projectDir, "package.json"),
+  JSON.stringify(projectPackageJson, null, 2)
+);
+
+console.log("Success! Your new project is ready.");
+console.log(`Created ${projectName} at ${projectDir}`);
